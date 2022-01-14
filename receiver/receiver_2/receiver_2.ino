52388b175631baa84c8019d2006929710923284a
hazartaylan <57994196+hazartaylan@users.noreply.github.com>
2022-01-14 22:59:43 +0300

    Add files via upload

---
+++ receiver/receiver_2/receiver_2.ino
@@ -0,0 +1,81 @@
+#include <SoftwareServo.h>
+#include <SoftwareSerial.h>
+#include <VirtualWire.h>
+#include <ServoTimer2.h>
+
+//#define DEBUG_MODE
+
+ServoTimer2 myservo1;
+SoftwareServo myservo2;
+SoftwareServo myservo3;
+SoftwareServo myservo4;
+
+const int numberOfAnalogPins = 4; // how many analog integer values to receive
+int data[numberOfAnalogPins]; // the data buffer
+int value[numberOfAnalogPins];
+// the number of bytes in the data buffer
+const int dataBytes = numberOfAnalogPins * sizeof(int);
+byte msgLength = dataBytes;
+
+void setup() {
+  myservo1.attach(9);  //Propeller
+  myservo2.attach(10);  //Rudder
+  myservo3.attach(8);  // Ailron
+  myservo4.attach(12);   // Elevator
+
+  Serial.begin(9600);
+
+#ifdef DEBUG_MODE
+  Serial.println("Ready");
+#endif
+
+  // Initialize the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000); // Bits per sec
+  vw_set_rx_pin(11);  
+  vw_rx_start(); // Start the receiver
+}
+
+
+void loop(){
+
+  if (vw_get_message((byte*)data, &msgLength))  {    // Non-blocking
+
+#ifdef DEBUG_MODE  
+  Serial.println("Got: ");
+#endif
+
+  if(msgLength == dataBytes){
+  for (int i = 1; i < numberOfAnalogPins; i++) {
+
+#ifdef DEBUG_MODE    
+  Serial.print("pin ");
+  Serial.print(i);
+  Serial.print("=");
+  Serial.println(data[i]);
+#endif
+  value[0]=map(data[0],0,1023,1000,2000);
+  
+  value[i]=map(data[i],0,1023,0,179);  // Write into the servo
+  }
+  
+  myservo1.write(value[0]);
+  myservo2.write(value[1]);
+  myservo3.write(value[2]);
+  myservo4.write(value[3]);
+  delay(50);
+  SoftwareServo::refresh();   //refresh the servo
+  }
+  else {
+#ifdef DEBUG_MODE    
+  Serial.print("unexpected msg length of ");
+  Serial.println(msgLength);
+#endif
+  }
+#ifdef DEBUG_MODE      
+  Serial.println();
+#endif
+  }
+}
+
+
---
+++ transmitter/transmitter_2.ino
@@ -0,0 +1,27 @@
+#include <VirtualWire.h>
+const int numberOfAnalogPins = 4; // how many analog pins to read
+int data[numberOfAnalogPins]; // the data buffer
+const int dataBytes = numberOfAnalogPins * sizeof(int); 
+
+void setup() {
+//   pinMode(13, OUTPUT); //this is needed just to display the indication
+// Initialize the IO and ISR
+vw_set_ptt_inverted(true); // Required for DR3100
+vw_setup(2000); // Bits per sec
+}
+
+void loop() {
+int values = 0;
+// digitalWrite(13, HIGH); 
+for(int i=0; i <= numberOfAnalogPins; i++) {
+data[i] = analogRead(i); // store the values into the data buffer
+}
+send((byte*)data, dataBytes);
+delay(10); //send every second
+//digitalWrite(13, LOW);
+}
+
+void send (byte *data, int nbrOfBytes) {
+vw_send(data, nbrOfBytes);
+vw_wait_tx(); // Wait until the whole message is gone
+}
